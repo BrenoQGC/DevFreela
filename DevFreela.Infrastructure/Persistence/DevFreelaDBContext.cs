0522c7f40e866bd4e0525a0b04dece05525d2299
Breno Carvalho <brenoqcarvalho@gmail.com>
2024-07-23 20:08:08 -0300

    Criadas confgurações das entidades de tabelas

--- DevFreela.Infrastructure/Persistence/DevFreelaDBContext.cs
+++ DevFreela.Infrastructure/Persistence/DevFreelaDBContext.cs
@@ -14,7 +14,56 @@ namespace DevFreela.Infrastructure.Persistence
         public DbSet<User> Users { get; set; }
         public DbSet<Skill> Skills { get; set; }
         public DbSet<UserSkill> UserSkills { get; set; }
-
         public DbSet<ProjectComment> ProjectComments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Project>()
+                .HasKey(P => P.Id);
+
+            modelBuilder.Entity<Project>()
+                .HasOne(p => p.Freelancer)
+                .WithMany(p => p.FreelanceProjects)
+                .HasForeignKey(p => p.IdFreelancer)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Project>()
+                .HasOne(p => p.Client)
+                .WithMany(p => p.OwnedProjects)
+                .HasForeignKey(p => p.IdClient)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<User>()
+                .HasKey(u => u.Id);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Skills)
+                .WithOne()
+                .HasForeignKey(u => u.IdSkill)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<ProjectComment>()
+                .HasKey(c => c.Id);
+
+            modelBuilder.Entity<ProjectComment>()
+                .HasOne(c => c.Project)
+                .WithMany(c => c.Comments)
+                .HasForeignKey(c => c.IdProject)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<ProjectComment>()
+                .HasOne(c => c.User)
+                .WithMany(c => c.Comments)
+                .HasForeignKey(c => c.IdUser)
+                .OnDelete(DeleteBehavior.Restrict) ;
+
+            modelBuilder.Entity<Skill>()
+                .HasKey(P => P.Id);
+            modelBuilder.Entity<UserSkill>()
+                .HasKey(P => P.Id);
+            modelBuilder.Entity<UserSkill>()
+                .HasKey(P => P.Id);
+
+        }
     }
 }
